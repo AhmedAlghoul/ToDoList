5e50b1ac804d97cbb364ff2eed882228fe1859f1
AhmedALghoul <Ahmed.Alghoul98@gmail.com>
2020-12-21 01:28:30 +0200

    improvments and add lists ui

--- app/src/main/java/com/example/android/applicationlab/MainActivity.java
+++ app/src/main/java/com/example/android/applicationlab/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.android.applicationlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.lists);
     }
+
 }
