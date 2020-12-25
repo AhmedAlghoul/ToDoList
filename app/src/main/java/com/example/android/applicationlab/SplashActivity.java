e68f3dffcdf9e3dd69b4578c0c8d140ae0066377
AhmedALghoul <Ahmed.Alghoul98@gmail.com>
2020-12-26 01:45:41 +0200

    all interfaces have been added

--- app/src/main/java/com/example/android/applicationlab/SplashActivity.java
+++ app/src/main/java/com/example/android/applicationlab/SplashActivity.java
@@ -1,14 +1,26 @@
 package com.example.android.applicationlab;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    TextView nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        nextBtn = findViewById(R.id.nextBtn);
+
+        nextBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
