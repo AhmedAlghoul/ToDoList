b059059cce40c93597fbb79dd3c145e237f5a21c
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2020-12-30 16:41:56 +0200

    Update LoginActivity.java

--- app/src/main/java/com/example/android/applicationlab/LoginActivity.java
+++ app/src/main/java/com/example/android/applicationlab/LoginActivity.java
@@ -35,48 +35,6 @@ Button loginbutton;
                 startActivity(intent);
             }
         });
-//
-//        et_Email = findViewById(R.id.et_Email);
-//        et_Password = findViewById(R.id.et_Password);
-//        pc_loading = findViewById(R.id.pc_loading);
-//
-//        Button btn_Login = findViewById(R.id.btn_Login);
-//        btn_Login.setOnClickListener(view -> {
-//            Helpers.HideKeyboard(LoginActivity.this);
-//
-//            String email = et_Email.getText().toString().trim();
-//            String password = et_Password.getText().toString().trim();
-//            if (email.isEmpty()) {
-//                et_Email.setError("Please enter email");
-//                return;
-//            }
-//            if (password.isEmpty()) {
-//                et_Password.setError("Please enter password");
-//                return;
-//            }
-//            pc_loading.bringToFront();
-//            pc_loading.setVisibility(View.VISIBLE);
-//            btn_Login.setEnabled(false);
-//            firebaseAuth.signInWithEmailAndPassword(email, password)
-//                    .addOnCompleteListener(this, task -> {
-//                        pc_loading.setVisibility(View.GONE);
-//                        btn_Login.setEnabled(true);
-//                        if (task.isSuccessful()) {
-//                            Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
-//                            Intent intent = new Intent(LoginActivity.this, ListsActivity.class);
-//                            startActivity(intent);
-//                            finish();
-//                        } else {
-//                            Toast.makeText(LoginActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-//                        }
-//                    });
-//        });
-//
-//        TextView tv_register = findViewById(R.id.tv_register);
-//        tv_register.setOnClickListener(view -> {
-//            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-//            startActivity(intent);
-//            finish();
-//        });
+
     }
 }
